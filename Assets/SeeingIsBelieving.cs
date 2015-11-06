a62414368461c6dfd339e3183e3a3da4c79de63e
David Axelrod <daxaxelrod@gmail.com>
2015-11-06 01:11:04 -0500

    work done to the ray logic
    Made some more progress with the ray drawing where it is coming out of
    the face, granted only in one direction (forward) Collider with the
    building is working as expected but the next aspect of adding the text
    is a 4 step process but its totally doable. see you tomorrow ^_^

--- Assets/SeeingIsBelieving.cs
+++ Assets/SeeingIsBelieving.cs
@@ -41,8 +41,21 @@ public class SeeingIsBelieving : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+
+
+        /* //---------------------------------todo-------------------------------------------------------------------
+        
+        1) when something gets hit, add an empty child node
+        2) Attach new textmesh and add the textmesh.text equal to the title of the parent
+        3) transform.Translate the childnode closer to the camera
+        4) Don't allow the ray to go through more than one element
+        5) have the child node self destruct after 10 seconds
+        
         //if left mouse key is pressed, shoot a ray, if the ray hits anything, take the name of the hit 
         //item and return it as a mesh text
+
+        
+        */ //---------------------------------todo-------------------------------------------------------------------
 
 
 
@@ -56,7 +69,7 @@ public class SeeingIsBelieving : MonoBehaviour {
 
         //returns an error saying that the current expression denotes a method group
         //Debug.Log(GetType());
-
+        GameObject building = GameObject.Find("test_collider");
         //center eye anchor is a gameobject, not a camera
        GameObject mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
         //why they thought that was a good idea, i have no clue. Useful in other contexts as well though
@@ -65,9 +78,38 @@ public class SeeingIsBelieving : MonoBehaviour {
         if (Input.GetKey(KeyCode.A)) {
             //Debug.DrawRay(new Vector3(-11f, 9.6f, -49.58f), gameObject.transform.forward, Color.red, 10);
             // Debug.DrawRay(gameObject.transform.position, camera.transform.foward);
-            Ray rayToUse = new Ray(mainCamera.transform.position, transform.forward);
-            Debug.DrawRay(rayToUse.origin, rayToUse.direction * 500000, Color.red, raycastTimeLength);
-            Debug.Log("Ray Drawn");
+            Ray rayFromCamera = new Ray(mainCamera.transform.position, transform.forward);
+            RaycastHit hit;
+            if (Physics.Raycast( rayFromCamera, out hit) ) {
+                TextMesh newMeshToDestroyIn5s = mainCamera.AddComponent<TextMesh>();
+
+
+                //null object reference
+                //newMeshToDestroyIn5s.transform.parent = mainCamera.transform;
+                Debug.Log(newMeshToDestroyIn5s);
+                //Debug.Log(mainCamera.transform);
+                    
+
+
+                // a collision occued. Check if it's our plane object and create our cuve at the 
+                // collision point facing toward the collision normal
+
+
+
+                Collider buildingCollider = building.GetComponent<Collider>();
+
+                 
+                // thing was hit != !null
+                if (hit.collider == buildingCollider) {
+           //          Debug.Log("HOLY FUCK IT ACCTUALLY WORKS!!");  
+                    //hit happened
+
+                }
+             }
+            Debug.DrawRay(rayFromCamera.origin, rayFromCamera.direction * 500000, Color.red, raycastTimeLength);
+           // Debug.Log("Ray Drawn");
+           //Debug.Log(rayToUse.direction);
+
 
         }
             
