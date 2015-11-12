ca6348e31e5deed1e3ccb91f45e6417d21f6a332
David Axelrod <daxaxelrod@gmail.com>
2015-11-11 21:28:21 -0500

    Snow Logic and new sound to the couple
    Particle emittors are not enableble so its a little annoying to work with them.

--- Assets/guy1speech.cs
+++ Assets/guy1speech.cs
@@ -4,21 +4,34 @@ using System.Collections;
 public class guy1speech : MonoBehaviour {
 
     AudioSource[] buildingDescription;
+    private float timeDelay;
+    private float snowTimer;
+    private int counter = 0;
 
+
+    
 
     void OnTriggerEnter(Collider player)
     {
+        // snowTimer = Time.time;
+
+        // Debug.Log(snowTimer);
         buildingDescription = GetComponents<AudioSource>();
         buildingDescription[0].Play();
-        float timeDelay = 5;
+        timeDelay = 3f;
         for (int audioPos = 1; audioPos < buildingDescription.Length; audioPos++)
         {
             timeDelay += buildingDescription[audioPos].time;
             //wow that works well
             //basically can add as many audios to this as you want and it will play them one after another
             buildingDescription[1].PlayDelayed(timeDelay);
-            Debug.Log("your gonna be up for more than an hour");
+            
         }
+    }
+    void OnTriggerStay(Collider player)
+    {
+        // lets do it on the trigger of the sound rather than the time
+        //snowTimer = Time.time;
     }
 
 
@@ -31,11 +44,21 @@ public class guy1speech : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+        counter++;
+        Debug.Log(counter);
+        if (counter >= 500) {
+            // GameObject.FindGameObjectWithTag("Snow").SetActive(true);
+            // snowBall.SetActive(true);
+
+
+
+            //if (snowTimer > ( startTime + 7)) {
+            // Debug.Log("7 seconds have elapsed since the trigger has been hit");
+        }
 	}
 }
