da5bb9eeca79947e054d39cc2b611652157e224a
David Axelrod <daxaxelrod@gmail.com>
2015-11-12 15:53:52 -0500

    started game scripting
    for the fruit game

---
+++ Assets/GameControllerFruit.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControllerFruit : MonoBehaviour {
+
+    private GameObject[] fruitIntance;
+  //  private GameObject fruitIntance2;
+
+	// Use this for initialization
+   	void Start () {
+            if(fruitIntance == null) {
+            fruitIntance = GameObject.FindGameObjectsWithTag("fruitApple");
+            }
+
+        for (int i = 0; i < fruitIntance.Length; i++)
+        {
+
+            fruitIntance[i].transform.Translate(new Vector3(0, 1, 0 * Time.deltaTime), Space.Self);
+
+
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+
+
+
+    }
+}
