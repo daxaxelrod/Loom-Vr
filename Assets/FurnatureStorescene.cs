b540654aad72b98b15ec4a6805d6be5bff6af79a
David Axelrod <daxaxelrod@gmail.com>
2015-11-19 16:22:24 -0500

    added furniture store and linked it to new scene

---
+++ Assets/FurnatureStorescene.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FurnatureStorescene : MonoBehaviour {
+
+    private bool hitTrigger = false;
+
+    void OnTriggerEnter() {
+        hitTrigger = true;
+    }
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (hitTrigger) {
+            Application.LoadLevel(4);
+        }
+
+	}
+}
