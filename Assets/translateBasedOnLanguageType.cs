46650b46760c075c890c63bb589a571dcf411417
David Axelrod <daxaxelrod@gmail.com>
2015-11-19 17:02:33 -0500

    Added furniture store and new scene for gamification

---
+++ Assets/translateBasedOnLanguageType.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class translateBasedOnLanguageType : MonoBehaviour {
+    //several ideas here. Link to google translate api
+    //find what language the user wants. To be built
+    // store all texts in textmeshes into large array
+    // maybe all of the sound clips? no idea
+    // then let google rip on them.
+    // set the current text mesh to the language that google provided
+
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
