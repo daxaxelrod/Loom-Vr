b33985a4928a6a0d5c6c0570e319b5ca8bff2aaf
David Axelrod <daxaxelrod@gmail.com>
2015-11-18 23:09:24 -0500

    Added money scripting, taught team about code base
    The scripting behind money is ready to be added to. Time for the player to make bank. On top of that, Jack and joe for a little while got to see a bit of the code base and install structure and practices that I'm using. I believe in this so much and i can't wait for others to start making this code base extraordinary. To the future :+1:

---
+++ Assets/EuroCounterMainOVR.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EuroCounterMainOVR : MonoBehaviour {
+
+    public double euros = 0;
+    private TextMesh euroCounterText;
+
+	// Use this for initialization
+	void Start () {
+        euroCounterText = gameObject.GetComponentInChildren<TextMesh>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (euros != 0)
+        {
+            // say they have the updated euro count
+        }
+        else {
+            // get the text in the child text mesh
+        }
+	
+	}
+}
---
+++ Assets/testScipttodestrot.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testScipttodestrot : MonoBehaviour {
+
+    // VARIABLE 
+    private GameObject buildingToMove;
+    private bool hitTrigger = false;
+
+    void OnTriggerEnter(Collider player) {
+
+        hitTrigger = true;
+        
+    }
+    void OnTriggerStay() {
+
+    }
+    void OnTriggerExit(Collider player) {
+        hitTrigger = false;
+    }
+    void OnCollisionEnter(Collision hit) {
+        
+    }
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+        buildingToMove = GameObject.FindGameObjectWithTag("learningAndBuildingMoveYolo");
+        
+	}
+	
+
+
+	// Update is called once per frame
+	void Update () {
+        //        gameObject
+
+        if (hitTrigger) {
+            buildingToMove.transform.Translate(new Vector3(0, 1, 0) * Time.deltaTime, Space.Self);
+        }
+        //    if (Input.GetKey(KeyCode.Space)) { 
+        //buildingToMove.transform.Translate(new Vector3(0,5,0)* Time.deltaTime, Space.Self);
+        //}
+
+    }
+}
