06798e1cd60219ad1811072016a11a96563fe62c
David Axelrod <daxaxelrod@gmail.com>
2015-11-30 23:23:10 -0500

    Added furniture and scripting to furniture store
    still dreading the creation of a universal backpack. but the scripts are
    ripe to throw right into an array. very happy with the first day back

---
+++ Assets/BuyChairScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuyChairScript : MonoBehaviour {
+
+    private bool hitTrigger = false;
+    private TextMesh chairBuyTextMesh;
+
+    void OnTriggerEnter(Collider player) {
+        hitTrigger = true;
+    }
+    void OnTriggerExit(Collider player) {
+        hitTrigger = false;
+    }
+
+	// Use this for initialization
+	void Start () {
+           chairBuyTextMesh = gameObject.GetComponentInChildren<TextMesh>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+
+        if (hitTrigger) {
+            chairBuyTextMesh.text = "This chair costs 35 Euros\n Press enter to purchase";
+            if (Input.GetKey(KeyCode.Return)) {
+                //warning message
+                chairBuyTextMesh.text = "Are you sure? \n press enter to continue";
+                if (Input.GetKey(KeyCode.Return)) {
+                    chairBuyTextMesh.text = "It's all yours!";
+                }
+            }
+
+        }
+
+	
+	}
+}
---
+++ Assets/BuyTable.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuyTable : MonoBehaviour {
+
+    private bool hitTrigger = false;
+    private TextMesh tableTextMesh;
+
+    void OnTriggerEnter(Collider player) {
+        hitTrigger = true;
+    }
+
+    void OnTriggerExit(Collider player) {
+        hitTrigger = false;
+    }
+
+    // Use this for initialization
+    void Start () {
+        tableTextMesh = gameObject.GetComponentInChildren<TextMesh>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (hitTrigger) {
+            tableTextMesh.text = "If you'd like to buy this table\n press enter";
+            if (Input.GetKey(KeyCode.Return)) {
+                //message
+                tableTextMesh.text = "Are you sure?";
+                if (Input.GetKey(KeyCode.Return)) {
+                    tableTextMesh.text = "Enjoy!!";
+                    // add table to the backpack
+                  //  gameObject.transform.FindChild("table")
+                }
+            }
+        }
+	
+	}
+}
