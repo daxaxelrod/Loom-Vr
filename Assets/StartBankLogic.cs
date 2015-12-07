a0cd369beefba437777057199135637329ce244a
David Axelrod <daxaxelrod@gmail.com>
2015-12-06 20:19:08 -0500

    Added bank logic and a tent city additive
    Good day. going to continue add to the bank game logic and make sure that transactions are taken from global scope

---
+++ Assets/StartBankLogic.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartBankLogic : MonoBehaviour {
+
+    private EuroCounterMainOVR transactionTracker;
+    private TextMesh BankTextMesh;
+    private AudioSource BankAudioSource;
+    
+    void OnTriggerEnter(Collider player)
+    {
+       transactionTracker = player.GetComponent<EuroCounterMainOVR>();
+    }
+
+	// Use this for initialization
+	void Start () {
+       //  transactionTracker.euros;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/tentCityAdditive.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tentCityAdditive : MonoBehaviour {
+    
+    void OnTriggerEnter(Collider player) {
+        Application.LoadLevelAdditive(6);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	 void Update () {}
+}
