0baac734adacf84a4fbb7373330e1a555ea8a26e
David Axelrod <daxaxelrod@gmail.com>
2015-12-10 17:00:37 -0500

    Functional Start Menu
    Added a menu that either starts loom or quits the application. Fun stuff

---
+++ Assets/menuScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class menuScript : MonoBehaviour {
+
+    public Canvas quitMenu;
+    public Button startText;
+    public Button exitText;
+
+
+	// Use this for initialization
+	void Start () {
+        quitMenu = quitMenu.GetComponent<Canvas>();
+        startText = startText.GetComponent<Button>();
+        exitText = exitText.GetComponent<Button>();
+        quitMenu.enabled = false;
+	}
+
+
+    public void ExitPress() {
+        quitMenu.enabled = true;
+        startText.enabled = false;
+        exitText.enabled = false;
+        Debug.Log("You pressed exit");
+
+        }
+
+    public void NoPress() {
+        quitMenu.enabled = false;
+        startText.enabled = true;
+        exitText.enabled = true;
+        
+    }
+
+    public void StartLoom() {
+        Application.LoadLevel(0);
+    }
+    public void ExitLoom() {
+        Application.Quit();
+
+    }
+
+
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
