be6de692640d9ca27cb5c9543452673aa93dd8ee
David Axelrod <daxaxelrod@gmail.com>
2015-12-12 18:26:25 -0500

    Lot more work on the gamify fruit scene
    Also some imports as well including itween (pathfinder plugin) and some city prefabs. alex and jack and i talked about the game and alex helped out a lot

---
+++ Assets/ITweenTestPath.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ITweenTestPath : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("TestPath"), "time", 10));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/WithParentMoveToTarget.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WithParentMoveToTarget : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject parentBitch;
+
+    [SerializeField]
+    private Transform target;
+
+    // POSSIBLE FOR MULTI TARGETED PATHFINDING
+    //[SerializeField]
+    //private Transform[] target;
+    //private int x;
+
+
+
+    private NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+        agent = gameObject.GetComponent<NavMeshAgent>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // x = target.GetLength(0);
+        // for () { }
+        agent.SetDestination(target.position);
+        // NOTE updated position take the current position of the angent
+        //parentBitch.transform.position = gameObject.transform.position;
+    }
+    //void FixedUpdate()
+    //{
+        
+    //    parentBitch.transform.position = gameObject.transform.position;
+    //}
+
+}
