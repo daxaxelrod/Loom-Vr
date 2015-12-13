4e2ca959d5ed0b40b3f721dafa6803a6304b35a7
David Axelrod <daxaxelrod@gmail.com>
2015-12-13 16:17:53 -0500

    Worked on rayCasting for the carnival game
    coming along decently well. Need to write code for moving the actual target. Think that basic networking is there but have no idea how to expand the game dynamically for multiple players

--- Assets/RaiseBarInBack.cs
+++ Assets/RaiseBarInBack.cs
@@ -4,19 +4,37 @@ using System.Collections;
 public class RaiseBarInBack : MonoBehaviour {
 
     [SerializeField]
-    public ParticleEmitter emmiter;
+    private Camera camera;
+    //      public ParticleEmitter emmiter;
+    private Ray ray;
+    // Use this for initialization
+    void Start()
+    {
+        // we are not using a particle emmiter. whoops
+        //   emmiter = (ParticleEmitter) Instantiate( emmiter, transform.position, Quaternion.identity);
 
-	// Use this for initialization
-	void Start () {
-        emmiter = (ParticleEmitter) Instantiate( emmiter, transform.position, Quaternion.identity);
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        for (int i = 0; i < 2; i++) {
+
+        RaycastHit hit;
+        ray = camera.ScreenPointToRay(Vector3.forward);
+        if (Physics.Raycast(ray, out hit))
+        {
+            Transform objectHit = hit.transform;
+            Debug.Log(string.Format("You hit {0} jackass", objectHit.name.ToString()));
+            //objectHit.Translate(new Vector3(0, 15 * Time.deltaTime, 0));
+
+            Transform playerCube = gameObject.transform.GetChild(0);
+            if (playerCube != null ) { }
+            playerCube.localScale.Set(100f, 0, 0);
 
         }
-	
+    }
+        // Update is called once per frame
+        void Update () {
+        //for (int i = 0; i < 2; i++) {
+
+        //}
+
+        Debug.DrawRay(ray.origin, ray.direction, Color.blue, 10f);
+        
 	}
 }
