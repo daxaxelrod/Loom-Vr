b904f97d4701a6fdafe3011e7fe5f517aa9ce293
David Axelrod <daxaxelrod@gmail.com>
2015-12-13 15:47:49 -0500

    did some work with google cardboard
    hopefully be able to have the small demo for people to try before they buy the system done during winter break. key to get people to try it out

---
+++ Assets/FruitGameMach2.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class FruitGameMach2 : MonoBehaviour {
+
+    private GameObject fruit;
+    private int fruitCount;
+    public float spawnWait;
+    public float startWait;
+    public float birdWait;
+
+    public GUIText scoreText;
+    private int score;
+
+
+	// Use this for initialization
+	void Start () {
+        score = 0;
+        UpdateScore();
+        
+	}
+
+    private void UpdateScore()
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator SpawnWaves() {
+        yield return new WaitForSeconds(startWait);
+    }
+	
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
