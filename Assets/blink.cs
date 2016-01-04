c9ff1b736df0699cb6118dc1bf81477b67cb2639
David Axelrod <daxaxelrod@gmail.com>
2016-01-03 22:20:56 -0500

    added initial opening scene
    story > special effects..... yes?

---
+++ Assets/blink.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class blink : MonoBehaviour {
+
+    private GameObject topLid;
+    private GameObject bottomLid;
+
+	// Use this for initialization
+	void Start () {
+        topLid = GameObject.Find("blinkUpper");
+        bottomLid = GameObject.Find("blinkLower");
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            topLid.GetComponent<Animation>().Play("blinkUpper");
+            bottomLid.GetComponent<Animation>().Play("blinkLower");
+
+        }
+	}
+}
